4c6dd8a35b9b99164f5e8800b5eeff641d080f8f
Caio Vieira <contato@caiovieira.com.br>
2022-04-08 21:12:23 -0300

    test: add jest styled components

---
+++ src/pages/_document.tsx
@@ -0,0 +1,39 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const originalRenderPage = ctx.renderPage
+
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component
+      })
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    return await Document.getInitialProps(ctx)
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
