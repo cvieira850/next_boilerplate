ab1c62d372c36ba2f79884ea8156dd8075d43c57
Caio Vieira <contato@caiovieira.com.br>
2022-04-11 18:20:00 -0300

    chore: pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
 }
 
 module.exports = nextConfig
+module.exports = withPWA(nextConfig)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,12 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="white"
+        ></meta>
         <meta
           name="description"
           content="A simple project starter to work with TypeScript and React
