ed1569e20527ec2326bb5caca338d01c2d97f01b
Caio Vieira <contato@caiovieira.com.br>
2022-04-11 08:34:14 -0300

    chore: add storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, Story } from '@storybook/react'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'React Avançado',
+    description: 'TypeScript, ReactJS, NextJS e Styled Components'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'title basic',
+  description: 'description basic'
+}
+
+export const Default: Story = (args) => <Main {...args} />
